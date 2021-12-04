b9d16cd97d6098262509db3369f566dbea8059fb
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-04 19:26:38 +0530

    TODO 1: Done with configuring Hire Wheel Project

---
+++ hirewheel/src/main/java/com/hirewheel/hirewheel/HirewheelApplication.java
@@ -0,0 +1,14 @@
+package com.hirewheel.hirewheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewheelApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HirewheelApplication.class, args);
+	}
+
+}
